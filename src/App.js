a1fa421832dfe09ae80866ea385f85f36022ce94
jp8433 <jaypatel.in9167@gmail.com>
2022-12-05 18:58:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes,Route, BrowserRouter} from 'react-router-dom'; 
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import InvoiceForm from './components/InvoiceForm';
+import Login from './components/Login'
+import Final from './components/Final'
+
+class App extends Component {
+  render() {
+  return (
+   // <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+      
+      //  <InvoiceForm/>
+        <BrowserRouter>
+        <Routes>
+        <Route exact path="/" element={<InvoiceForm/>}/> 
+        <Route exact path="/Final" element={<Final/>}/> 
+          <Route exact path="/Login" element={<Login/>}/> 
+        </Routes>
+        </BrowserRouter>
+   
+    //</div>
+  );
+}}
+
+export default App;
---
+++ src/components/Final.js
@@ -0,0 +1,68 @@
+
+import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {useNavigate,useParams} from 'react-router-dom'
+import axios from 'axios';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import { NavLink } from 'react-router-dom'
+
+
+
+function Final(){
+  let navigate = useNavigate()
+  let params = useParams()
+  let [email,setEmail] = useState("")
+  let [password,setpassword] = useState("")
+  
+  let handleSubmit = async()=>{
+    console.log("jay")
+    try { 
+      let res = await axios.get(`https://6380fd1e8efcfcedac14e348.mockapi.io/reactapp`)
+      .then(function (response) {
+          // sessionStorage.setItem('token',response.data.access_token)
+          console.log(email, password)
+          let resData = response.data
+          for(let data of resData) {
+            console.log(data)
+            if(data.email == email && data.password == password) {
+               // navigate('/all-users')
+               window.location.assign("src\components\Errror.js")
+            }
+          }
+          // console.log(sessionStorage.getItem('token'));
+          console.log('Next Page ')
+        }
+      )
+    }catch(error)
+    {
+      console.log("jay in err")
+    }      
+     
+  }
+  return <>
+   <Navbar bg="dark" variant="dark">
+                <Container>
+                    <NavLink to="/" className="text-decoration-none text-light mx-2">User Login</NavLink>
+                   
+                </Container>
+            </Navbar>
+    <Form>
+      <Form.Group className="mb-3" >
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter login email"/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" value={password} onChange={(e)=>setpassword(e.target.value)} placeholder="Enter login password"/>
+      </Form.Group>
+      <Button variant="primary" onClick={()=>handleSubmit()}>
+        Submit
+      </Button>
+    </Form>
+</>
+}
+export default Final
